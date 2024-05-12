ca03cebd9b02c0201f1887dc5d21bad3406dec95
Huỳnh Nam Duy <119779697+Dahlia3003@users.noreply.github.com>
2024-05-13 03:44:16 +0700

    first

---
+++ src/main/java/org/rocket/wygo/WygoApplication.java
@@ -0,0 +1,13 @@
+package org.rocket.wygo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WygoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WygoApplication.class, args);
+	}
+
+}
